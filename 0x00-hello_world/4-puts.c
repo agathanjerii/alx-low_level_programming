8e31b5afa6c7365897975e9faa20ac6c90fe2138
agathanjerii <agkimani18@gmail.com>
2023-06-19 11:17:12 -0700

    Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ * return zero if no error and non zero if error is found
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
